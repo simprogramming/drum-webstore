6b3444c94f1ba7c3461334ebc8d8cb1440924e85
Simon Leiva <leivaprogramming@gmail.com>
2020-07-01 11:33:13 -0400

    finished fondation of back end

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,13 @@ class ItemsController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def edit
+  end
+
+  def update
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_150853) do
+ActiveRecord::Schema.define(version: 2020_06_25_204919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "brand"
+    t.string "details"
+    t.string "condition"
+    t.string "price"
+    t.string "pictures"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
