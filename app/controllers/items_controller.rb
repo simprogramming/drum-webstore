c74753fd4e583c79a960ef296112d2a2834c2020
Simon Leiva <leivaprogramming@gmail.com>
2020-07-01 13:02:01 -0400

    most of back end done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,47 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(item)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @item.update(item_params)
+    redirect_to items_path
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category, :brand, :details, :condition, :price, :pictures)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'cleaning database'
+Item.destroy_all
+puts 'creating your library of items'
+
+category = ['drum', 'gear']
+brand = ['brand1', 'brand2']
+details = ['size1', 'size2', 'size3']
+condition = ['new', 'good', 'bad']
+items_attributes = [
+  {
+    name: Faker::FunnyName.name,
+    category: category.sample,
+    brand: brand.sample,
+    details: details.sample,
+    condition: condition.sample,
+    price: Faker::Number.digit,
+    pictures: "drumset.png"
+  },
+  {
+    name: Faker::FunnyName.name,
+    category: category.sample,
+    brand: brand.sample,
+    details: details.sample,
+    condition: condition.sample,
+    price: Faker::Number.digit,
+    pictures: "drumset.png"
+  },
+  {
+    name: Faker::FunnyName.name,
+    category: category.sample,
+    brand: brand.sample,
+    details: details.sample,
+    condition: condition.sample,
+    price: Faker::Number.digit,
+    pictures: "drumset.png"
+  }
+]
+items_attributes.each { |params| Item.create!(params)}
