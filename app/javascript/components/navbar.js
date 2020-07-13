0c03428ebb7e8d54b65adcba7ceadf816b886302
Simon Leiva <leivaprogramming@gmail.com>
2020-07-12 21:56:21 -0400

    prototype done

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,12 +2,12 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-drumstore');
   window.addEventListener('scroll', () => {
     if (navbar) {
-      if (window.scrollY >= window.innerHeight*0.70) {
+      if (window.scrollY >= window.innerHeight*0.65) {
         navbar.classList.add('navbar-drumstore-black');
-        navbar.classList.add('navbar-drumstore-black a');
+        // navbar.classList.add('navbar-drumstore-black a');
       } else {
         navbar.classList.remove('navbar-drumstore-black');
-        navbar.classList.remove('navbar-drumstore-black a');
+        // navbar.classList.remove('navbar-drumstore-black a');
       }
     }
   });
